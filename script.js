05de8d3cd1e4d3630d21f5d2b5edd6fa7621a985
Sayan-712 <134630207+Sayan-712@users.noreply.github.com>
2023-08-25 02:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius=document.getElementById("radius");
+	let rad=radius.volume;
+	const volume=document.getElementById("volume");
+  volume.value=3.14*rad*rad*rad;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
